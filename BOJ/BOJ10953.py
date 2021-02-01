0ed9b0bd40aec5d6829d9053c5dd43d8220236a0
eunhye <ehye826@gmail.com>
2021-02-01 21:51:48 +0900

    BOJ1953 A+B-6 (문자열)

---
+++ BOJ/BOJ10953.py
@@ -0,0 +1,10 @@
+'''
+    @date : 2021-02-01
+    @source : https://www.acmicpc.net/problem/10953
+    @email : ehye826@gmail.com
+'''
+
+
+for i in range(0, int(input())):
+    print(sum(map(int, input().split(','))))
+    
