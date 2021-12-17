6a4c49ad930b4277f5d57b70449f3da6aad80dbb
eunhye <ehye826@gmail.com>
2021-12-17 12:06:13 +0900

    167. Two Sum II - Input Array Is Sorted (Array)

---
+++ Leetcode/167-Two-Sum-II-Input-Array-Is-Sorted.js
@@ -0,0 +1,21 @@
+/*
+* @source https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+* @author EunHyeJung
+* @email ehye826@gmail.com
+* @date 2021.12.16
+*/
+var rotate = function(nums, k) {
+    let n = nums.length;
+    k = k > n ? k % n : k;
+    let cursor = n - k, i = 0;
+    let tmp = nums.slice(cursor, n);
+
+    while (i < cursor) {
+      nums[n- 1- i] = nums[cursor - 1 - i];
+      i += 1;
+    }
+    tmp.forEach((num, i) => {
+      nums[i] = tmp[i];
+    });
+
+};
