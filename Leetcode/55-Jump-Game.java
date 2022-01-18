b1b587a9f16b274d42e91aa0183405339b56c2a2
eunhye <ehye826@gmail.com>
2022-01-18 17:58:31 +0900

    55. Jump Game (Array, DP, Greedy)

--- Leetcode/55-Jump-Game.java
+++ Leetcode/55-Jump-Game.java
@@ -4,29 +4,16 @@
 * @email ehye826@gmail.com
 * @date 2021.01.17
 */
-
 class Solution {
-    private boolean[] visited;
     public boolean canJump(int[] nums) {
         int n = nums.length;
         if (n == 1) return true;
-        this.visited = new boolean[n];
-        return jump(nums, 0);
-    }
-
-    public boolean jump(int[] nums, int curIdx) {
-        if (curIdx >= nums.length - 1)  return true;
-        int dist = nums[curIdx];
-        while(dist > 0) {
-            if (dist + curIdx >= nums.length - 1)   
-                return true;
-            if (!visited[dist + curIdx]) {
-                visited[dist + curIdx] = true;
-                if (jump(nums, dist + curIdx))
-                    return true;
-            }
-            dist -= 1;
+        
+        int maxI = nums[0];
+        for(int i = 1 ; i < n; i++) {
+            if (i > maxI)   return false;
+            maxI = Math.max(maxI, i + nums[i]);
         }
-        return false;
+        return true;
     }
 }
