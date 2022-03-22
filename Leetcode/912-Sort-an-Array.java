b9117bf39752d16050dcafb3a1cdd366cf4e2ffc
eunhye <ehye826@gmail.com>
2022-03-22 23:24:10 +0900

    912. Sort an Array (Array, Sort)

---
+++ Leetcode/912-Sort-an-Array.java
@@ -0,0 +1,12 @@
+/*
+* @source https://leetcode.com/problems/sort-an-array/
+* @author EunHyeJung
+* @email ehye826@gmail.com
+* @date 2022.03.22
+*/
+class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return  nums;
+    }
+}
