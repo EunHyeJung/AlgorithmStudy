6f7bd0d0af85ece10ee690ac636e67190504cf83
eunhye <ehye826@gmail.com>
2022-05-02 16:21:32 +0900

    204. Count Primes (Array, Math)

--- Leetcode/204-Count-Primes.java
+++ Leetcode/204-Count-Primes.java
@@ -9,16 +9,17 @@ class Solution {
         int cnt = 0;
         if (n < 2)  return cnt;
         boolean[] chk = new boolean[n + 1];
-        for (int i = 2; i <= n / 2; i++) {
+        int m = (int) Math.sqrt(n);
+        for (int i = 2; i <= m; i++) {
             if (chk[i])
                 continue;
-            cnt += 1;
             for (int j = 2; i * j < n; j++) {
                 chk[i * j] = true;
             }
         }
-        for (int i = n / 2 + 1; i < n; i++) {
-            if (!chk[i])    cnt += 1;
+        for (int i = 2; i < n; i++) {
+            if (!chk[i])
+                cnt += 1;
         }
         return cnt;
     }
