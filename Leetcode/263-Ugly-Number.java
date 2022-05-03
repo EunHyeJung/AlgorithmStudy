adb58f44dfc4dc972c4521b530a55c35632b0c18
eunhye <ehye826@gmail.com>
2022-05-03 22:39:20 +0900

    263. Ugly Number (Math)

--- Leetcode/263-Ugly-Number.java
+++ Leetcode/263-Ugly-Number.java
@@ -6,18 +6,14 @@
 */
 class Solution {
     public boolean isUgly(int n) {
-        if (n <= 0)  return false;
-        if (n == 1) return true;
-        int i = 2;
-        int m = (int) Math.sqrt(n);
-        while(i <= m) {
-            while (n % i == 0) {
-                if (!(i == 2 || i == 3 || i == 5))
-                    return false;
-                n /= i;
-            }
-            i += 1;
-        }
-        return n == 1 || n % 2 == 0 || n % 3 == 0 || n % 5 == 0;
+        if (n <= 0) return false;
+        while (n % 2 == 0)
+            n /= 2;
+        while (n % 3 == 0)
+            n /= 3;
+        while (n % 5 == 0)
+            n /= 5;
+        return n == 1;
     }
 }
+
