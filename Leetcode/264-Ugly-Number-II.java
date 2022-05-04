0bdfc13e11e729c3a2d4016c5607043de30f70b7
eunhye <ehye826@gmail.com>
2022-05-04 16:43:10 +0900

    264. Ugly Number II (Math, Priority Queue)

--- Leetcode/264-Ugly-Number-II.java
+++ Leetcode/264-Ugly-Number-II.java
@@ -8,10 +8,9 @@ class Solution {
     private static final int MAX_VALUE_1 = Integer.MAX_VALUE / 2;
     private static final int MAX_VALUE_2 = Integer.MAX_VALUE / 3;
     private static final int MAX_VALUE_3 = Integer.MAX_VALUE / 5;
-    
+
     public int nthUglyNumber(int n) {
         Queue<Integer> uglyNumQueue = new PriorityQueue<>();
-
         uglyNumQueue.add(1);
         int count = 0;
 
@@ -20,12 +19,19 @@ class Solution {
             count += 1;
             if (count == n)
                 return curNum;
-            if (curNum <= MAX_VALUE_1 && !uglyNumQueue.contains(curNum * 2))
-                uglyNumQueue.add(curNum * 2);
-            if (curNum <= MAX_VALUE_2 && !uglyNumQueue.contains(curNum * 3))
-                uglyNumQueue.add(curNum * 3);
-            if (curNum <= MAX_VALUE_3 && !uglyNumQueue.contains(curNum * 5))
-                uglyNumQueue.add(curNum * 5);
+
+            int nextNum = curNum * 2;
+            if (curNum <= MAX_VALUE_1) {
+                uglyNumQueue.add(nextNum);
+            }
+            nextNum = curNum * 3;
+            if (curNum <= MAX_VALUE_2 && nextNum % 2 != 0) {
+                uglyNumQueue.add(nextNum);
+            }
+            nextNum = curNum * 5;
+            if (curNum <= MAX_VALUE_3 && nextNum % 2 != 0 && nextNum % 3 != 0) {
+                uglyNumQueue.add(nextNum);
+            }
         }
 
         return 0;
