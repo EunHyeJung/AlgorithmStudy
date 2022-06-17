3b62fb4e94b390603339964398582c44bfe1f774
eunhye <ehye826@gmail.com>
2022-06-17 19:34:48 +0900

    5. Longest Palindromic Substring (DP)

--- Leetcode/5-Longest-Palindromic-Substring.java
+++ Leetcode/5-Longest-Palindromic-Substring.java
@@ -2,34 +2,32 @@
 * @source https://leetcode.com/problems/longest-palindromic-substring/
 * @author EunHyeJung
 * @email ehye826@gmail.com
-* @date 2022.01.10
+* @date 2022.06.17
 */
 class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() == 1) return s;
-        String maxSubstr = Character.toString(s.charAt(0));
         int n = s.length();
-        boolean[][] chk = new boolean[n][n];
-        for(int k = 0; k < n; k++) {
-            for(int i = 0; i < n; i++) {
-                if (i + k > n - 1) break;
-                if (k == 0) {
-                    chk[i][i + k] = true;
-                } else if (k == 1) {
-                    chk[i][i + k] = chk[i + k][i] = s.charAt(i) == s.charAt(i + k);
-                    if(chk[i][i + k]) {
-                        maxSubstr = s.substring(i, i + k + 1);
-                    }
+        if (n == 1)
+            return s;
+            
+            
+        boolean[][] dp = new boolean[n][n];
+        int st = -1, end = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (s.charAt(i) == s.charAt(j)
+                        && (i - j <= 2 || dp[i - 1][j + 1])) {
+                    dp[i][j] = true;
                 }
-                else {
-                    if (i + 1 < n && chk[i + 1][i + k - 1] && s.charAt(i) == s.charAt(i + k)) {
-                        chk[i][i + k] = true;
-                        maxSubstr = s.substring(i, i + k + 1);
-                    }
+
+                if (dp[i][j]) {
+                   if ((st == -1 && end == -1) || i - j + 1 > end - st) {
+                       end = i + 1;
+                       st = j;
+                   }
                 }
             }
         }
-
-        return maxSubstr;
+        return s.substring(st ,end);
     }
 }
